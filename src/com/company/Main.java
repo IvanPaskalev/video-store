7ebc7278744ae849f505cee9ec7f8b6ba66980da
IvanPaskalev <ivanpaskalev93@gmail.com>
2021-11-21 20:50:55 +0200

    "add" functionality halfway

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hi");
-        System.out.println("hi");
+        System.out.println("Добър ден!\nАко изкате да запишете нов филм, натиснете 1\nАко искате да намерите вече записан филм, натиснете 2\nАко искате да излезете от програмата, натиснете 3");
+        switch (new Scanner(System.in).nextInt()){
+            case 1:
+                System.out.println("enter movie title");
+                Movie movie = new Movie();
+                while (true){
+                    String title = new Scanner(System.in).nextLine();
+                    if (!title.isEmpty()){
+                        movie.setTitle(title);
+                        break;
+                    } else {
+                        System.out.println("No input entered");
+                    }
+                }
+
+                break;
+
+            case 2:
+                break;
+        }
     }
 }
