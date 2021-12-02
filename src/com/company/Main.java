ded3bd289be44c594fe8e738cd328c0962bbd89e
IvanPaskalev <ivanpaskalev93@gmail.com>
2021-12-02 18:42:40 +0200

    case 2 func/halfway

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,70 +7,109 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Добър ден!\nАко изкате да запишете нов филм, натиснете 1\nАко искате да намерите вече записан филм, натиснете 2\nАко искате да излезете от програмата, натиснете 3");
-        switch (new Scanner(System.in).nextInt()){
-            case 1:
-                System.out.println("enter movie title");
-                Movie movie = new Movie();
-                while (true){
-                    String title = new Scanner(System.in).nextLine();
-                    if (!title.isEmpty()){
-                        movie.setTitle(title);
-                        break;
-                    } else {
-                        System.out.println("No input entered");
+        while (true) {
+            System.out.println("Добър ден!\nАко изкате да запишете нов филм, натиснете 1\nАко искате да намерите вече записан филм, натиснете 2\nАко искате да излезете от програмата, натиснете 3");
+            boolean isQuiting = false;
+            switch (new Scanner(System.in).nextInt()) {
+                case 1:
+                    System.out.println("enter movie title");
+                    System.out.println("enter movie title");
+                    Movie movie = new Movie();
+                    while (true) {
+                        String title = new Scanner(System.in).nextLine().replaceAll(" ", "_!_");
+                        if (!title.isEmpty()) {
+                            movie.setTitle(title);
+                            break;
+                        } else {
+                            System.out.println("No input entered");
+                        }
                     }
-                }
-                System.out.println("Enter year");
-                while (true){
-                    int year = new Scanner(System.in).nextInt();
-                    if (year != 0){
-                        movie.setYear(year);
-                        break;
-                    } else {
-                        System.out.println("No input entered");
+                    System.out.println("Enter year");
+                    while (true) {
+                        int year = new Scanner(System.in).nextInt();
+                        if (year != 0) {
+                            movie.setYear(year);
+                            break;
+                        } else {
+                            System.out.println("No input entered");
+                        }
                     }
-                }
-                System.out.println("enter genre");
-                while (true){
-                    String genre = new Scanner(System.in).nextLine();
-                    if (!genre.isEmpty()){
-                        movie.setGenre(genre);
-                        break;
-                    } else {
-                        System.out.println("No input entered");
+                    System.out.println("enter genre");
+                    while (true) {
+                        String genre = new Scanner(System.in).nextLine();
+                        if (!genre.isEmpty()) {
+                            movie.setGenre(genre);
+                            break;
+                        } else {
+                            System.out.println("No input entered");
+                        }
                     }
-                }
-                BufferedReader bufferedReader = null;
-                String movieAsString = movie.toString();
-                try {
-                    bufferedReader = new BufferedReader(new FileReader("dataBase.txt"));
+                    String movieAsString = movie + "\n";
                     try {
                         Files.write(Paths.get("dataBase.txt"), movieAsString.getBytes(), StandardOpenOption.APPEND);
                         System.out.println("Movie saved");
-                    } catch (IOException e) {
+
+                    } catch (FileNotFoundException e) {
+                        try {
+                            Files.write(Paths.get("dataBase.txt"), movieAsString.getBytes());
+                            System.out.println("Movie saved");
+                        } catch (IOException ex) {
+                            System.out.println("Error! Could not save the movie.");
+                        }
+                    } catch (IOException exception) {
                         System.out.println("Error! Could not save the movie.");
                     }
-
-                } catch (FileNotFoundException e) {
-                    try {
-                        Files.write(Paths.get("dataBase.txt"), movieAsString.getBytes());
-                        System.out.println("Movie saved");
-                    } catch (IOException ex) {
-                        System.out.println("Error! Could not save the movie.");
+                    break;
+                case 2:
+                    System.out.println("If you want to search title press 1\nIf you want to search year press 2\nIf you want to search genre press 3");
+                    switch (new Scanner(System.in).nextInt()) {
+                        case 1:
+                            System.out.println("Enter name");
+                            String name = new Scanner(System.in).nextLine();
+                            try {
+                                List<String> lines = Files.readAllLines(Paths.get("dataBase.txt"));
+                                List<Movie> movies = new ArrayList<>();
+                                for (String line : lines) {
+                                    String[] info = line.split(" ");
+                                    int index = Integer.parseInt(info[0]);
+                                    String name1 = info[1].replaceAll("_!_", " ");
+                                    int year1 = Integer.parseInt(info[2].trim());
+                                    String genre1 = info[3];
+                                    movies.add(new Movie(index, name1, year1, genre1));
+                                }
+                                for (Movie singleMovie : movies) {
+                                    if (singleMovie.getTitle().contains(name)) {
+                                        System.out.println(singleMovie);
+                                    }
+                                }
+                            } catch (IOException e) {
+                                System.out.println("Nothing found");
+                                break;
+                            }
+                            break;
+                        case 2:
+                            System.out.println("Enter year");
+                            break;
+                        case 3:
+                            System.out.println("Enter genre");
+                            break;
                     }
-                }
-
+                    break;
+                case 3:
+                    isQuiting = true;
+                    break;
+            }
+            if (isQuiting) {
                 break;
-
-            case 2:
-                break;
+            }
         }
     }
 }
--- src/com/company/Movie.java
+++ src/com/company/Movie.java
@@ -84,3 +84,4 @@ public class Movie {
                 " " + isRented;
     }
 }
+
